82a4ac902e36c220bccdc3cd8f736cc3d84e75dd
Ernani Padilha <ernani@wj.com.br>
2023-06-22 09:24:34 -0300

    Implementação de métodos Put, GetAll e Get

--- src/main/java/com/padilha/produtos/controllers/ProductController.java
+++ src/main/java/com/padilha/produtos/controllers/ProductController.java
@@ -1,8 +1,17 @@
 package com.padilha.produtos.controllers;
-
+import com.padilha.produtos.dtos.ProductRecordDto;
+import com.padilha.produtos.models.ProductModel;
 import com.padilha.produtos.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ProductController {
@@ -10,5 +19,24 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts(){
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+
+    @GetMapping("products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id){
+        Optional<ProductModel> product0 = productRepository.findById(id);
+        if(product0.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(product0.get());
+    }
 }
--- src/main/java/com/padilha/produtos/dtos/ProductRecordDto.java
+++ src/main/java/com/padilha/produtos/dtos/ProductRecordDto.java
@@ -1,6 +1,9 @@
 package com.padilha.produtos.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
-public record ProductRecordDto(String name, BigDecimal value) {
+public record ProductRecordDto(@NotBlank String nome, @NotNull BigDecimal value) {
 }
